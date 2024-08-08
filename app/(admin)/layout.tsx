ec9bd0f0fb464b337329c06285d1c58d5ea2b147
Lu1sR0 <luiscardoso946@mail.com>
2024-08-08 15:22:34 -0300

    adicionei o studio e consertei alguns erros

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,25 @@
+import CmsNavbar from "../components/CMSNavbar";
+import { Provider } from "../utils/Provider";
+import "./globals.css";
+
+export const metadata = {
+  title: "Studio do Leo",
+  description: "Local onde você pode criar seus posts",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <Provider>
+          <CmsNavbar />
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 import { ThemeProvider } from "next-themes";
-import { Provider } from "./utils/Provider";
+import { Provider } from "../utils/Provider";
 
 //const inter = Inter({ subsets: ["latin"] });
 const firaCode = Fira_Code({ subsets: ["latin"] });
